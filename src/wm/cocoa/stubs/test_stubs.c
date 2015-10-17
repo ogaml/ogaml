dc0ed2a07d39ea81f3e3f47cde0dbfa3b16a6d74
Sheeft <isheeft@gmail.com>
2015-10-17 20:00:58 +0200

    Attemps at creating a window

--- src/wm/cocoa/stubs/test_stubs.c
+++ src/wm/cocoa/stubs/test_stubs.c
@@ -3,11 +3,30 @@
 #include <caml/callback.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
-#include <caml/mlvalues.h> 
+#include <caml/mlvalues.h>
 
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
 static NSAutoreleasePool* arp = nil;
+
+@interface MyApplication : NSApplication
+
+@property (strong, nonatomic) NSWindow *window;
+
+@end
+
+@implementation MyApplication
+
+- (void) applicationDidFinishLaunching: (NSNotification *) note
+{
+    NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(100, 100, 100, 100)
+                        styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:YES];
+
+    self.window = window;
+}
+
+@end
 
 CAMLprim value
 caml_init_arp(value unit)
@@ -54,6 +73,22 @@ caml_cocoa_print_string(value str)
   CAMLparam1(str);
 
   NSLog (@"%@", (NSString*)str);
+
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value
+caml_open_window(value unit)
+{
+  CAMLparam0();
+
+  // NSRect frame = NSMakeRect(0, 0, 200, 200);
+  // NSWindow* window  = [[[NSWindow alloc] initWithContentRect:frame
+  //                     styleMask:NSBorderlessWindowMask
+  //                     backing:NSBackingStoreBuffered
+  //                     defer:NO] autorelease];
+  // [window setBackgroundColor:[NSColor blueColor]];
+  // [window makeKeyAndOrderFront:NSApp];
 
   CAMLreturn(Val_unit);
 }
