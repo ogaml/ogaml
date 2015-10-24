a345b4b6905f77e562d8a36b54501738c44fe276
VLanvin <victor.lanvin@ens-cachan.fr>
2015-10-24 11:56:23 +0200

    More matrices

--- src/math/matrix3f.ml
+++ src/math/matrix3f.ml
@@ -92,6 +92,14 @@ let product m1 m2 =
   m.{15} <- m1.{3} *. m2.{12} +. m1.{7} *. m2.{13} +. m1.{11} *. m2.{14} +. m1.{15} *. m2.{15};
   m
 
+let transpose m' = 
+  let m = create () in
+  m.{0}  <- m'.{0}; m.{1}  <- m'.{4}; m.{2}  <- m'.{8} ; m.{3}  <- m'.{12};
+  m.{4}  <- m'.{1}; m.{5}  <- m'.{5}; m.{6}  <- m'.{9} ; m.{7}  <- m'.{13};
+  m.{8}  <- m'.{2}; m.{9}  <- m'.{6}; m.{10} <- m'.{10}; m.{11} <- m'.{14};
+  m.{12} <- m'.{3}; m.{13} <- m'.{7}; m.{14} <- m'.{11}; m.{15} <- m'.{15};
+  m
+
 let translate v m = product (translation v) m
 
 let scale v m = product (scaling v) m
@@ -105,5 +113,70 @@ let times m v =
     y = v.x *. m.{1} +. v.y *. m.{5} +. v.z *. m.{9} +. m.{12};
     z = v.x *. m.{2} +. v.y *. m.{6} +. v.z *. m.{10} +. m.{12}
   }
+
+let look_at ~from ~at ~up = 
+  let open Vector3f in
+  let dir = sub from at in
+  let right = normalize (cross up dir) in
+  let up = normalize (cross dir right) in
+  let m = identity () in
+  set 0 0 m (right.x);
+  set 1 0 m (right.y);
+  set 2 0 m (right.z);
+  set 0 1 m (up.x);
+  set 1 1 m (up.y);
+  set 2 1 m (up.z);
+  set 0 2 m (dir.x);
+  set 1 2 m (dir.y);
+  set 2 2 m (dir.z);
+  set 0 3 m (-. (dot from right));
+  set 1 3 m (-. (dot from up ));
+  set 2 3 m (-. (dot from dir));
+  m
+
+let orthographic ~right ~left ~near ~far ~top ~bottom =
+  let m = identity () in
+  set 0 0 m (2. /. (right -. left));
+  set 1 1 m (2. /. (top -. bottom));
+  set 2 2 m (2. /. (near -. far));
+  set 0 3 m ((right +. left) /. (right -. left));
+  set 1 3 m ((top +. bottom) /. (top -. bottom));
+  set 2 3 m ((far +. near) /. (far -. near));
+  m
+
+let iorthographic ~right ~left ~near ~far ~top ~bottom =
+  let m = identity () in
+  set 0 0 m ((right -. left) /. 2.);
+  set 1 1 m ((top -. bottom) /. 2.);
+  set 2 2 m ((near -. far) /. 2.);
+  set 0 3 m (-. (right +. left) /. 2.);
+  set 1 3 m (-. (top +. bottom) /. 2.);
+  set 2 3 m ((far +. near) /. 2.);
+  m
+
+let perspective ~near ~far ~width ~height ~fov =
+  let aspect = width /. height in
+  let top = (tan (fov /. 2.)) *. near in
+  let right = top *. aspect in 
+  let m = zero () in
+  set 0 0 m (near /. right);
+  set 1 1 m (near /. top);
+  set 2 2 m ((far +. near) /. (near -. far));
+  set 2 3 m (2. *. far *. near /. (near -. far));
+  set 3 2 m (-1.);
+  m
+
+let iperspective ~near ~far ~width ~height ~fov =
+  let aspect = width /. height in
+  let top = (tan (fov /. 2.)) *. near in
+  let right = top *. aspect in 
+  let m = zero () in
+  set 0 0 m (right /. near);
+  set 1 1 m (top /. near);
+  set 3 3 m ((far +. near) /. (2. *. far *. near));
+  set 2 3 m (-1.);
+  set 3 2 m ((near -. far) /. (2. *. far *. near));
+  m
+
 
 
--- src/math/matrix3f.mli
+++ src/math/matrix3f.mli
@@ -27,6 +27,9 @@ val rotation : Vector3f.t -> float -> t
 (* Product *)
 val product : t -> t -> t
 
+(* Transposition *)
+val transpose : t -> t
+
 (* Return a new, translated matrix *)
 val translate : Vector3f.t -> t -> t
 
@@ -38,5 +41,24 @@ val rotate : Vector3f.t -> float -> t -> t
 
 (* Vector right-product *)
 val times : t -> Vector3f.t -> Vector3f.t
+
+(* Look-At view matrix *)
+val look_at : from:Vector3f.t -> at:Vector3f.t -> up:Vector3f.t -> t
+
+(* Orthographic projection matrix *)
+val orthographic : right:float -> left:float -> near:float -> far:float ->
+    top:float -> bottom:float -> t
+
+(* Inverse orthographic projection matrix *)
+val iorthographic : right:float -> left:float -> near:float -> far:float ->
+    top:float -> bottom:float -> t
+
+(* Perspective projection matrix *)
+val perspective : near:float -> far:float -> width:float -> height:float ->
+    fov:float -> t
+
+(* Inverse perspective projection matrix *)
+val iperspective : near:float -> far:float -> width:float -> height:float ->
+    fov:float -> t
 
 
