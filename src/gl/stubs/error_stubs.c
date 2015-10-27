f0a6523503502e6b37321546bdbbaf2bfe53be61
VLanvin <victor.lanvin@ens-cachan.fr>
2015-10-27 16:26:30 +0100

    removed stack errors under OSX

--- src/gl/stubs/error_stubs.c
+++ src/gl/stubs/error_stubs.c
@@ -51,13 +51,15 @@ caml_gl_get_error(value unit)
       res = Val_some(Val_int(4));
       break;
 
-    case GL_STACK_UNDERFLOW :
-      res = Val_some(Val_int(5));
-      break;
+    #ifndef __APPLE__
+      case GL_STACK_UNDERFLOW :
+        res = Val_some(Val_int(5));
+        break;
 
-    case GL_STACK_OVERFLOW :
-      res = Val_some(Val_int(6));
-      break;
+      case GL_STACK_OVERFLOW :
+        res = Val_some(Val_int(6));
+        break;
+    #endif
 
     default:
       res = Val_none;
