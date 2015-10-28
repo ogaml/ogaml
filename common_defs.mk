fe57226246a499b083764d7c0140ed4280a139b8
VLanvin <victor.lanvin@ens-cachan.fr>
2015-10-28 12:49:17 +0100

    added compat mode to Makefile

--- common_defs.mk
+++ common_defs.mk
@@ -5,16 +5,28 @@ OS_NAME =
 OS_WIN_STUBS =
 OS_WIN_STUBS_DIR =
 OS_WIN_STUBS_TEST =
+PP_DEFINE = 
 
 UNAME := $(shell uname)
 
+COMPATIBILITY_MODE = True
+
 ifeq ($(UNAME), Linux)
   OS_NAME = LINUX
-  OS_WIN_STUBS = xlib_stubs
-  OS_WIN_STUBS_DIR = xlib/
-  OS_WIN_STUBS_TEST = xtest_glcube
+  ifeq ($(COMPATIBILITY_MODE), True)
+    PP_DEFINE = __OSX__
+    OS_WIN_STUBS = cocoa_stubs
+    OS_WIN_STUBS_DIR = cocoa/
+    OS_WIN_STUBS_TEST = ctest_simple
+  else 
+    PP_DEFINE = __LINUX__
+    OS_WIN_STUBS = xlib_stubs
+    OS_WIN_STUBS_DIR = xlib/
+    OS_WIN_STUBS_TEST = xtest_glcube
+  endif
 endif
 ifeq ($(UNAME), Darwin)
+  PP_DEFINE = __OSX__
   OS_NAME = OSX
   OS_WIN_STUBS = cocoa_stubs
   OS_WIN_STUBS_DIR = cocoa/
@@ -56,7 +68,7 @@ CCOMPIL_FLAGS = -Wall -D_FILE_OFFSET_BITS=64 -D_REENTRANT -fPIC -I '$(OCAML_DIR)
 
 # Commands
 
-PPCOMMAND = cppo -D '__$(strip $(OS_NAME))__'
+PPCOMMAND = cppo -D '$(strip $(PP_DEFINE))'
 
 DEPCOMMAND = $(OCAMLDEP) -pp "$(PPCOMMAND)" $(INCLUDE_DIRS)
 
