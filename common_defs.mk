40ee192285b924bff60b09a42a86e7aa897f4cda
TheoWinterhalter <isheeft@gmail.com>
2015-11-08 11:31:56 +0100

    Fixed cocoa compiling

--- common_defs.mk
+++ common_defs.mk
@@ -1,8 +1,8 @@
 
 # OS-dependant constants
 
-OS_NAME = 
-PP_DEFINE = 
+OS_NAME =
+PP_DEFINE =
 OS_WIN_LIB =
 GLOBAL_OBJCOPTS =
 GLOBAL_CLIBS =
@@ -18,17 +18,17 @@ ifeq ($(UNAME), Linux)
     OS_WIN_LIB = cocoa
     GLOBAL_OBJCOPTS = $(shell gnustep-config --objc-flags)
     GLOBAL_CLIBS = $(shell gnustep-config --gui-libs)
-  else 
+  else
     PP_DEFINE = __LINUX__
     OS_WIN_LIB = x11
-    GLOBAL_OBJCOPTS = 
+    GLOBAL_OBJCOPTS =
     GLOBAL_CLIBS = -lX11 -lGL
   endif
 endif
 ifeq ($(UNAME), Darwin)
   PP_DEFINE = __OSX__
   OS_NAME = OSX
-  OS_WIN_LIB = cocoa 
+  OS_WIN_LIB = cocoa
   GLOBAL_OBJCOPTS = -fconstant-string-class=NSConstantString
   GLOBAL_CLIBS = -framework Foundation -framework Cocoa -framework Carbon -lobjc -framework openGL
 endif
@@ -71,4 +71,3 @@ DEPCOMMAND = $(OCAMLDEP) -pp "$(PPCOMMAND)" $(INCLUDE_DIRS)
 # Suffixes
 
 .SUFFIXES: .ml .mli .cmo .cmi .cmx .c .o .a .mllib .clib .m
-
