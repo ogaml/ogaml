8c4c523781dafc4bd3350b4bc2ffef9fce8d138a
Victor Lanvin <victor.lanvin@ens-cachan.fr>
2016-10-26 21:16:20 +0200

    Implemented context bindings

--- src/audio/stubs/context_stubs.c
+++ src/audio/stubs/context_stubs.c
@@ -9,4 +9,52 @@
 #include "utils.h"
 
 
+CAMLprim value
+caml_alc_create_context(value device)
+{
+  CAMLparam1(device);
 
+  CAMLreturn((value)alcCreateContext((ALCdevice*)device,NULL));
+}
+
+CAMLprim value
+caml_alc_make_current_context(value context)
+{
+  CAMLparam1(context);
+
+  ALCboolean res;
+
+  res = alcMakeContextCurrent((ALCcontext*)context);
+
+  CAMLreturn(Val_bool(res == ALC_TRUE));
+}
+
+CAMLprim value
+caml_alc_process_context(value context)
+{
+  CAMLparam1(context);
+
+  alcProcessContext((ALCcontext*)context);
+
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value
+caml_alc_suspend_context(value context)
+{
+  CAMLparam1(context);
+
+  alcSuspendContext((ALCcontext*)context);
+
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value
+caml_alc_destroy_context(value context)
+{
+  CAMLparam1(context);
+
+  alcDestroyContext((ALCcontext*)context);
+
+  CAMLreturn(Val_unit);
+}
