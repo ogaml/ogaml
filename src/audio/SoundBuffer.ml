40a48138d9fdbe47b50af2416c5c0acbec751da2
Théo Winterhalter <isheeft@gmail.com>
2016-10-27 19:59:47 +0200

    Placeholder SoundBuffer

--- src/audio/SoundBuffer.ml
+++ src/audio/SoundBuffer.ml
@@ -1,1 +1,18 @@
- 
+
+type t = unit
+
+type samples = (int, Bigarray.int16_signed_elt, Bigarray.c_layout) Bigarray.Array1.t
+
+let load s = ()
+
+let create ~samples ~channels ~rate = ()
+
+let duration buff = 0.
+
+let samples buff = assert false
+
+module LL = struct
+
+  let buffer buff = assert false
+
+end
