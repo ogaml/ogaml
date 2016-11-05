c25e57393a609f62c9846dc6c9c926a98cd1885f
Théo Winterhalter <isheeft@gmail.com>
2016-11-05 16:11:57 +0100

    Progress in SoundBuffer

--- src/audio/SoundBuffer.ml
+++ src/audio/SoundBuffer.ml
@@ -1,20 +1,32 @@
 
-type t = unit
-
 type samples = (int, Bigarray.int16_signed_elt, Bigarray.c_layout) Bigarray.Array1.t
 
-let load s = ()
+type t = {
+  buffer   : AL.Buffer.t ;
+  duration : float ;
+  samples  : samples ;
+  channels : [`Stereo | `Mono]
+}
 
-let create ~samples ~channels ~rate = ()
+let load s = assert false
 
-let duration buff = 0.
+let create ~samples ~channels ~rate =
+  let buffer = AL.Buffer.create () in
+  let data = AL.ShortData.of_bigarray samples in
+  begin match channels with
+  | `Stereo -> AL.Buffer.data_stereo
+  | `Mono   -> AL.Buffer.data_mono
+  end buffer data (AL.ShortData.length data) rate ;
+  { buffer ; duration = 0. ; samples ; channels }
 
-let samples buff = assert false
+let duration buff = buff.duration
 
-let channels buff = `Mono
+let samples buff = buff.samples
+
+let channels buff = buff.channels
 
 module LL = struct
 
-  let buffer buff = assert false
+  let buffer buff = buff.buffer
 
 end
