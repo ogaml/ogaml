0eee5847f54832db116afd7657bb73ebcd5b41dc
Théo Winterhalter <isheeft@gmail.com>
2019-01-14 15:04:03 +0000

    lengths function to compute lengths of different arrays from AST

--- src/graphics/model2.ml
+++ src/graphics/model2.ml
@@ -17,6 +17,7 @@ type face = face_point * face_point * face_point
   The idea is to stick as close as possible to OBJ file.
 
   TODO: Lines, materials, groups, object names, colors(?).
+  TODO: Quads?
 *)
 type t = {
   vertices : Vector3f.t array ;
@@ -27,13 +28,27 @@ type t = {
 
 let transform m obj = {
   obj with
-  vertices = Array.map (Matrix3D.times m) vertices ;
-  normals  = Array.map (Matrix3D.times m) normals ;
-  uv       = Array.map (Matrix3D.times m) uv
+  vertices = Array.map (Matrix3D.times m) obj.vertices ;
+  normals  = Array.map (Matrix3D.times m) obj.normals
 }
 
 (* TODO More efficient rotate/scale/translate? *)
 
 (* TODO to_vertex_array or add_to_vertex_array perhaps *)
 
-(* TODO from_obj *)
+(* Building from an OBJ file *)
+let rec lengths vn nn uvn fn ast =
+  let open ObjAST in
+  match ast with
+  | Vertex v :: ast -> lengths (vn + 1) nn uvn fn ast
+  | UV v :: ast -> lengths vn nn (uvn + 1) fn ast
+  | Normal v :: ast -> lengths vn (nn + 1) uvn fn ast
+  | Tri _ :: ast -> lengths vn nn uvn (fn + 1) ast
+  | Quad _ :: ast (* TODO perhaps? *)
+  | Param :: ast
+  | Mtllib _ :: ast
+  | Usemtl _ :: ast
+  | Object _ :: ast
+  | Group  _ :: ast
+  | Smooth _  :: ast -> lengths vn nn uvn fn ast
+  | [] -> (vn, nn, uvn, fn)
