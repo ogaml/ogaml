d09ba6735c55842feb76cfaf1e63c94a8bf4e479
Victor Lanvin <victor.lanvin@irif.fr>
2020-09-04 14:53:27 +0200

    Updated Travis scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,13 +2,11 @@
 
 OPAM_DEPEND="menhir dune"
 
-sh -e /etc/init.d/xvfb start
-
-export ppa=avsm/ocaml42+opam12
-
-sudo add-apt-repository -y ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers opam
+sudo apt-get -qq --force-yes install libgl1-mesa-dev libgl1-mesa-glx mesa-common-dev libglapi-mesa libgbm1 libgl1-mesa-dri libxatracker-dev
+sudo apt-get -qq --force-yes install libglew-dev freeglut3-dev libxi-dev libxmu-dev xserver-xorg-video-dummy xpra xorg-dev opencl-headers libgl1-mesa-dev
+sudo apt-get -qq --force-yes install libxcursor-dev libpulse-dev libxinerama-dev libxrandr-dev libx11-xcb-dev libopenal-dev libxv-dev libasound2-dev libudev-dev mesa-utils libgl1-mesa-glx
 
 export OPAMYES=1
 opam init --compiler=$OCAML_VERSION
@@ -18,8 +16,8 @@ opam install ${OPAM_DEPEND}
 
 make
 make install
-make tests
-make examples
+xvfb-run --auto-servernum --server-num=1 make tests
+xvfb-run --auto-servernum --server-num=1 make examples
 make uninstall
 make clean
-glxinfo
+xvfb-run --auto-servernum --server-num=1 glxinfo
