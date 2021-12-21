e82d65a7f1c33d8f7b781a1b47011b7d28ff53a7
Victor Lanvin <victor.lanvin2@gmail.com>
2021-12-21 22:23:57 +0100

    Moved matrix multiplication to C code (5x speedup)

---
+++ src/math/stubs/utils.h
@@ -0,0 +1,22 @@
+#ifndef CAML_STUBS_HEADER
+#define CAML_STUBS_HEADER
+
+#define CAML_NAME_SPACE
+
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/callback.h>
+#include <caml/memory.h>
+#include <caml/alloc.h>
+#include <caml/mlvalues.h>
+#include <stdio.h>
+
+#define Val_none Val_int(0)
+
+#define Some_val(v) Field(v,0)
+
+value Val_some(value v);
+
+value Int_pair(int a, int b);
+
+#endif
